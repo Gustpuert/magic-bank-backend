d3294f40c14509191dffd6e9b00f230dbab4b141
Gustavo Puerta Ramirez <gustavopuerta@yahoo.com>
2025-12-17 14:16:26 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+index.jsconst express = require("express");
+const cors = require("cors");
+
+const studentRoutes = require("./api/students");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ status: "Magic Bank Backend activo" });
+});
+
+app.use("/api", studentRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
