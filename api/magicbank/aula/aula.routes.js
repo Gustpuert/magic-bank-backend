1cbf291552e32ab1094ff65a8bd0e354e838a19f
Gustavo Puerta Ramirez <gustavopuerta@yahoo.com>
2025-12-22 15:54:37 -0500

    Delete api/magicbank/aula/aula.routes.js

--- api/magicbank/aula/aula.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { handleAulaMessage } = require("./aula.controller");
-
-router.post("/message", handleAulaMessage);
-
-module.exports = router;
