585f868603146942af55bbb76872a37b20320292
Gustavo Puerta Ramirez <gustavopuerta@yahoo.com>
2025-12-24 07:36:52 -0500

    Update aula.routes.js

--- api/magicbank/aula/aula.routes.js
+++ api/magicbank/aula/aula.routes.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const controller = require("./aula.controller");
 
 router.post("/texto", controller.aulaTexto);
-router.post("/voz", controller.aulaVoz);
 
 module.exports = router;
