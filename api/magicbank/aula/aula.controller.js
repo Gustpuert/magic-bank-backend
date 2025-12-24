ecc862c372cf68aa149d4439c1025e8600ce074e
Gustavo Puerta Ramirez <gustavopuerta@yahoo.com>
2025-12-24 07:35:47 -0500

    Update aula.controller.js

--- api/magicbank/aula/aula.controller.js
+++ api/magicbank/aula/aula.controller.js
@@ -1,5 +1,4 @@
 const aulaService = require("./aula.service");
-const speechService = require("../../services/speech.service");
 
 async function aulaTexto(req, res) {
   try {
@@ -10,28 +9,6 @@ async function aulaTexto(req, res) {
   }
 }
 
-async function aulaVoz(req, res) {
-  try {
-    const { audio_base64, course_id, profile } = req.body;
-
-    const texto = await speechService.speechToText(audio_base64);
-
-    const result = await aulaService.runAula({
-      message: texto,
-      course_id,
-      profile,
-    });
-
-    res.json({
-      ...result,
-      texto_detectado: texto,
-    });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-}
-
 module.exports = {
   aulaTexto,
-  aulaVoz,
 };
