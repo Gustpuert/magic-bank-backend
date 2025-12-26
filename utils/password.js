0d4e1689d52f924d15377e010256ab401fc8263a
Gustavo Puerta Ramirez <gustavopuerta@yahoo.com>
2025-12-26 08:43:59 -0500

    Update password.js

--- utils/password.js
+++ utils/password.js
@@ -1,1 +1,17 @@
+const crypto = require("crypto");
 
+function hashPassword(password) {
+  return crypto
+    .createHash("sha256")
+    .update(password)
+    .digest("hex");
+}
+
+async function verifyPassword(password, hash) {
+  return hashPassword(password) === hash;
+}
+
+module.exports = {
+  hashPassword,
+  verifyPassword
+};
