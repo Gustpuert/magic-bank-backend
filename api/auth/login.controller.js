18b277fd3eac56f27d028ed9aaba235ef5bcee2d
Gustavo Puerta Ramirez <gustavopuerta@yahoo.com>
2025-12-26 08:42:39 -0500

    Update login.controller.js

--- api/auth/login.controller.js
+++ api/auth/login.controller.js
@@ -1,1 +1,26 @@
+const loginService = require("./login.service");
 
+async function login(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        error: "Credenciales incompletas"
+      });
+    }
+
+    const result = await loginService.login({ email, password });
+
+    return res.json(result);
+
+  } catch (error) {
+    return res.status(401).json({
+      error: error.message
+    });
+  }
+}
+
+module.exports = {
+  login
+};
