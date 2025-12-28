49dfdeb9887d1dbf41df4f9e966617cc2da90feb
Gustavo Puerta Ramirez <gustavopuerta@yahoo.com>
2025-12-27 19:35:47 -0500

    Update auth.middleware.js

--- api/auth/auth.middleware.js
+++ api/auth/auth.middleware.js
@@ -1,9 +1,21 @@
-/**
- * Middleware placeholder
- * (listo para JWT o roles después)
- */
+const jwt = require("jsonwebtoken");
+
 function authMiddleware(req, res, next) {
-  next();
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: "Token requerido" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: "Token inválido" });
+  }
 }
 
 module.exports = authMiddleware;
