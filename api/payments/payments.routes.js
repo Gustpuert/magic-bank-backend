0c9c377b1561fc363b7d98df4233c7dd0f709803
Gustavo Puerta Ramirez <gustavopuerta@yahoo.com>
2025-12-27 22:09:48 -0500

    Update payments.routes.js

--- api/payments/payments.routes.js
+++ api/payments/payments.routes.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const { paymentWebhook } = require("./payments.controller");
 
+/**
+ * WEBHOOK DE PAGO
+ * SOLO POST
+ */
 router.post("/webhook", paymentWebhook);
 
 module.exports = router;
