5be08f72ed9dc3856e8f3585b91c7d7387ab325e
Gustavo Puerta Ramirez <gustavopuerta@yahoo.com>
2025-12-27 23:53:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
-/**
- * MagicBank Backend
- * Auth + JWT
- */
-
 const express = require("express");
 const cors = require("cors");
 
 const authRoutes = require("./api/auth/auth.routes");
+const paymentRoutes = require("./api/payments/payments.routes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-/* Health */
 app.get("/", (req, res) => {
   res.status(200).send("OK");
 });
 
-/* AUTH */
 app.use("/api/auth", authRoutes);
+app.use("/api/payments", paymentRoutes);
 
-/* PORT */
 const PORT = process.env.PORT;
 
 if (!PORT) {
