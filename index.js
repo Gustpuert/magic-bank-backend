2eb5ed92ad1fc2649664fe8d59fa94c7d77c41a3
Gustavo Puerta Ramirez <gustavopuerta@yahoo.com>
2025-12-27 19:37:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,20 @@
-/**
- * MagicBank Backend
- */
-
 const express = require("express");
 const cors = require("cors");
 
 const authRoutes = require("./api/auth/auth.routes");
-const loginRoutes = require("./api/auth/login.routes");
-const paymentRoutes = require("./api/payments/payments.routes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-/* Health check */
 app.get("/", (req, res) => {
-  res.status(200).send("OK");
+  res.send("OK");
 });
 
-/* AUTH */
 app.use("/api/auth", authRoutes);
-app.use("/api/login", loginRoutes);
 
-/* PAYMENTS */
-app.use("/api/payments", paymentRoutes);
-
-/* PORT */
 const PORT = process.env.PORT;
-
 if (!PORT) {
   console.error("PORT no definido");
   process.exit(1);
