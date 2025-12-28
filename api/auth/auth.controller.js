3abfff0ca3a7aeb0a5ab0b039efa2114bab4edf3
Gustavo Puerta Ramirez <gustavopuerta@yahoo.com>
2025-12-27 22:50:50 -0500

    Update auth.controller.js

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -1,7 +1,10 @@
 function validateSession(req, res) {
+  const now = Math.floor(Date.now() / 1000);
+
   res.status(200).json({
     valid: true,
-    user: req.user
+    user: req.user,
+    expires_in: req.user.exp - now
   });
 }
 
