6d595174bdd39b089e6180f2514ec56b77a006f5
Gustavo Puerta Ramirez <gustavopuerta@yahoo.com>
2025-12-27 21:35:05 -0500

    Update auth.controller.js

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -1,26 +1,10 @@
-const authService = require("./auth.service");
+const { createUser } = require("./auth.service");
 
-function register(req, res) {
+exports.register = async (req, res) => {
   try {
-    const { email, password } = req.body;
-    authService.registerUser(email, password);
-    res.status(201).json({ message: "Usuario registrado" });
+    const user = await createUser(req.body);
+    res.status(201).json(user);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
-}
-
-function login(req, res) {
-  try {
-    const { email, password } = req.body;
-    const result = authService.loginUser(email, password);
-    res.json(result);
-  } catch (error) {
-    res.status(401).json({ error: error.message });
-  }
-}
-
-module.exports = {
-  register,
-  login
 };
