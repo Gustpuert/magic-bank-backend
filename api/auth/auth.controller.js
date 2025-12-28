7bd015a0b2261c7b7a839438ba9fe1276d6f3dd5
Gustavo Puerta Ramirez <gustavopuerta@yahoo.com>
2025-12-27 19:06:36 -0500

    Update auth.controller.js

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -1,9 +1,6 @@
 const authService = require("./auth.service");
 
-/**
- * REGISTER
- */
-async function registerUser(req, res) {
+async function register(req, res) {
   try {
     const { email, password } = req.body;
 
@@ -13,7 +10,7 @@ async function registerUser(req, res) {
       });
     }
 
-    const user = await authService.register(email, password);
+    const user = await authService.registerUser(email, password);
 
     return res.status(201).json({
       message: "Usuario registrado correctamente",
@@ -27,34 +24,6 @@ async function registerUser(req, res) {
   }
 }
 
-/**
- * LOGIN
- */
-async function loginUser(req, res) {
-  try {
-    const { email, password } = req.body;
-
-    if (!email || !password) {
-      return res.status(400).json({
-        error: "Email y password son obligatorios"
-      });
-    }
-
-    const user = await authService.login(email, password);
-
-    return res.status(200).json({
-      message: "Login exitoso",
-      user
-    });
-
-  } catch (error) {
-    return res.status(401).json({
-      error: error.message
-    });
-  }
-}
-
 module.exports = {
-  registerUser,
-  loginUser
+  register
 };
