fa585ef964785ae92576fadb8fd083211699d546
Gustavo Puerta Ramirez <gustavopuerta@yahoo.com>
2026-03-04 13:25:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3337,6 +3337,9 @@ res.status(500).json({error:err.message})
 /* =============================
 START
 ========================= */
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("MAGICBANK BACKEND ACTIVO EN PUERTO", PORT);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+console.log(`MagicBank backend running on port ${PORT}`);
 });
