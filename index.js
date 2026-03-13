62d6eeecbfb04f0841141eb802f433ced387ad1e
Gustavo Puerta Ramirez <gustavopuerta@yahoo.com>
2026-03-13 15:29:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2019,30 +2019,7 @@ res.status(500).send("Error instalando control de sesión");
 }
 
 });
-/* =========================================================
-TEMPORAL - INSTALAR BLOQUEO POR DISPOSITIVO
-========================================================= */
 
-app.get("/install-device-lock", async (req, res) => {
-
-try {
-
-await pool.query(`
-ALTER TABLE access_tokens
-ADD COLUMN IF NOT EXISTS device_hash TEXT;
-`);
-
-res.send("Bloqueo de dispositivo instalado");
-
-} catch (error) {
-
-console.error(error);
-
-res.status(500).send("Error instalando device lock");
-
-}
-
-});
 
 
 /* =========================================================
